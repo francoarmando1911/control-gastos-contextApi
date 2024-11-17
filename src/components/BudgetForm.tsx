dfe1100b1d10e5457cbcfab118c05bc2af9f5d2e
Franco Armando <francoarmando1911@gmail.com>
2024-11-17 20:23:22 -0300

    function isValid

--- src/components/BudgetForm.tsx
+++ src/components/BudgetForm.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useMemo, useState } from "react"
 
 
 export default function BudgetForm(){
@@ -10,8 +10,11 @@ export default function BudgetForm(){
         //console.log(e.target.id)
 
         setBudget(e.target.valueAsNumber)
-        
     }
+
+    const isValid = useMemo(() => {
+        return isNaN(budget) || budget <= 0
+    }, [budget])
 
     return(
         <form className="space-y-5">
@@ -33,8 +36,8 @@ export default function BudgetForm(){
             <input 
                 type="submit"
                 value='Definir presupuesto'
-                className="bg-blue-600 hover:bg-blue-700 cursor-pointer w-full p-2 text-white font-black uppercase" 
-                
+                className="bg-blue-600 hover:bg-blue-700 cursor-pointer w-full p-2 text-white font-black uppercase disabled:opacity-40" 
+                disabled={isValid}
             />
         </form>
     )
