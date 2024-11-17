41021583458f51fff8c46f3d078b88e32acb5c05
Franco Armando <francoarmando1911@gmail.com>
2024-11-17 20:04:43 -0300

    17/11

--- src/components/BudgetForm.tsx
+++ src/components/BudgetForm.tsx
@@ -2,6 +2,26 @@
 
 export default function BudgetForm(){
     return(
-        <div>BudgetForm</div>
+        <form className="space-y-5">
+            <div className="flex flex-col space-y-5">
+                <label htmlFor="budget" className="text-4xl text-blue-600 font-bold text-center">
+                    Definir presupuesto
+                </label>
+                <input
+                    id="budget"
+                    type="number"
+                    className="w-full bg-white border border-gray-200 p-2"
+                    placeholder="Define tu presupuesto"
+                    name="budget"
+                />
+            </div>
+
+            <input 
+                type="submit"
+                value='Definir presupuesto'
+                className="bg-blue-600 hover:bg-blue-700 cursor-pointer w-full p-2 text-white font-black uppercase" 
+                
+            />
+        </form>
     )
 }
