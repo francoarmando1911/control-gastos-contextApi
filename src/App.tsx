6944f9924626e461fef3c3e3eaf458237c44edfa
Franco Armando <francoarmando1911@gmail.com>
2024-11-17 21:23:12 -0300

    continuando budgetForm

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import BudgetForm from "./components/BudgetForm"
-import {useBudget} from "./hooks/useBudget"
+
 
 function App() {
 
-  const {state, dispatch} = useBudget()
-
-  console.log(state)
+  
 
   return (
     <>
--- src/components/BudgetForm.tsx
+++ src/components/BudgetForm.tsx
@@ -1,9 +1,10 @@
 import { useMemo, useState } from "react"
-
+import { useBudget } from "../hooks/useBudget"
 
 export default function BudgetForm(){
 
     const [budget, setBudget] = useState(0)
+    const {dispatch} = useBudget()
 
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         //console.log(e.target.valueAsNumber)
@@ -16,8 +17,14 @@ export default function BudgetForm(){
         return isNaN(budget) || budget <= 0
     }, [budget])
 
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        //console.log('Añadir o definir presupuesto: ')
+        dispatch({type: 'add-budget', payload: {budget}})
+    }
+
     return(
-        <form className="space-y-5">
+        <form className="space-y-5" onSubmit={handleSubmit}>
             <div className="flex flex-col space-y-5">
                 <label htmlFor="budget" className="text-4xl text-blue-600 font-bold text-center">
                     Definir presupuesto
