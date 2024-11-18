8301f83962825064dfaaa4ade53869531b69c168
Franco Armando <francoarmando1911@gmail.com>
2024-11-17 21:28:30 -0300

    budgetTracker

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import BudgetForm from "./components/BudgetForm"
-
+import BudgetTracker from "./components/BudgetTracker"
+import { useBudget } from "./hooks/useBudget"
+import { useMemo } from "react"
 
 function App() {
 
+  const {state} = useBudget()
+  //console.log(state.budget)
+
+  const isValidBudget = useMemo(() => state.budget > 0, [state.budget])
   
 
   return (
@@ -12,7 +18,7 @@ function App() {
       </header>
 
       <div className="max-w-3xl mx-auto bg-white shadow-lg rounded-lg mt-10 p-10">
-        <BudgetForm/>
+        {isValidBudget ? <BudgetTracker/> : <BudgetForm/>}
 
 
       </div>
