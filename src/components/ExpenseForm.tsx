21bcc6e19670136ae54284b815734e766ed94596
Franco Armando <francoarmando1911@gmail.com>
2024-11-17 23:35:43 -0300

    expenseform

--- src/components/ExpenseForm.tsx
+++ src/components/ExpenseForm.tsx
@@ -1,7 +1,37 @@
 
 
 export default function ExpenseForm() {
-  return (
-    <div>ExpenseForm</div>
-  )
+    return (
+        <form className="space-y-5">
+            <legend className="uppercase text-center text-2xl font-black border-b-4 border-blue-500 py-2">Nuevo gasto</legend>
+
+            <div className="flex flex-col gap-2">
+                <label
+                    htmlFor="expenseName"
+                    className="text-xl"
+                >Nuevo gasto:</label>
+                <input
+                    type="text"
+                    id="expenseName"
+                    placeholder="Añade el nombre del gasto: "
+                    className="bg-slate-100 p-2"
+                    name="expenseName"
+                />
+            </div>
+
+            <div className="flex flex-col gap-2">
+                <label
+                    htmlFor="amount"
+                    className="text-xl"
+                >Cantidad: </label>
+                <input
+                    type="number"
+                    id="amount"
+                    placeholder="Ingrese la cantidad del gasto ej. 1500: "
+                    className="bg-slate-100 p-2"
+                    name="amount"
+                />
+            </div>
+        </form>
+    )
 }
