a2ea639a28bf0a8de37ea8a3360d39bce02b2774
Franco Armando <francoarmando1911@gmail.com>
2024-11-18 20:40:46 -0300

    18/11

--- src/components/ExpenseDetail.tsx
+++ src/components/ExpenseDetail.tsx
@@ -1,4 +1,6 @@
+import { formatDate } from "../helpers"
 import { Expense } from "../types"
+import AmountDisplay from "./AmountDisplay"
 
 type ExpenseDetailProps = {
     expense: Expense
@@ -6,16 +8,19 @@ type ExpenseDetailProps = {
 
 export default function ExpenseDetail({expense} : ExpenseDetailProps) {
   return (
-    <div className="bg-white shadow-lg p-10 w-full border-b border-gray-200">
+    <div className="bg-white shadow-lg p-10 w-full border-b border-gray-200 flex gap-5 item-center">
         <div>
 
         </div>
 
         <div>
             <p>{expense.expenseName}</p>
-            <p className="text-slate-600 text-sm">{expense.date?.toString()}</p>
-
+            <p className="text-slate-600 text-sm">{formatDate(expense.date!.toString())}</p>
         </div>
+
+        <AmountDisplay
+            amount={expense.amount}
+        />
 
     </div>
   )
--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -2,7 +2,7 @@ export function formatCurrency(amount: number) {
     return new Intl.NumberFormat('es-AR', { style: 'currency', currency: 'ARS' }).format(amount);
 }
 
-export function fonrmatDate(dateStr: string) : string {
+export function formatDate(dateStr: string) : string {
     const dateObj = new Date(dateStr)
     const options : Intl.DateTimeFormatOptions = {
         weekday: 'long', 
