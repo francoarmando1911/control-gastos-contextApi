c82820caed2051f86ab9de447db9737237363532
Franco Armando <francoarmando1911@gmail.com>
2024-11-19 17:16:34 -0300

    19/11

--- src/components/ExpenseDetail.tsx
+++ src/components/ExpenseDetail.tsx
@@ -15,10 +15,14 @@ export default function ExpenseDetail({expense} : ExpenseDetailProps) {
   return (
     <div className="bg-white shadow-lg p-10 w-full border-b border-gray-200 flex gap-5 item-center">
         <div>
-
+          <img
+              src={`/icono_${categoryInfo.icon}.svg`}
+              alt="icono-gasto"
+              className="w-20"
+          />
         </div>
 
-        <div>
+        <div className="flex-1 space-y-2">
             <p className="text-sm font-bold uppercase text-slate-500">{categoryInfo.name}</p>
             <p>{expense.expenseName}</p>
             <p className="text-slate-600 text-sm">{formatDate(expense.date!.toString())}</p>
