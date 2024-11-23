7ed9bdeeef5cca7067aa22f251cd5a87f8b31212
Franco Armando <francoarmando1911@gmail.com>
2024-11-23 17:36:15 -0300

    manejo de epenses

--- src/components/BudgetTracker.tsx
+++ src/components/BudgetTracker.tsx
@@ -1,6 +1,13 @@
+import { useMemo } from "react";
+import { useBudget } from "../hooks/useBudget";
 import AmountDisplay from "./AmountDisplay";
 
 export default function BudgetTracker() {
+
+  const {state} = useBudget()
+
+  const totalExpenses = useMemo(() => state.expenses.reduce((total, expense) => expense.amount + total, 0),[state.expenses])
+
   return (
     <div className="grid grid-cols-1 md:grids-cols-2 gap-5">
         <div className="flex justify-center">
@@ -20,7 +27,7 @@ export default function BudgetTracker() {
 
             <AmountDisplay
                 label="Presupuesto"
-                amount={300}
+                amount={state.budget}
             />
 
             <AmountDisplay
