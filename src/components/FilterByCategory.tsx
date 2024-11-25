2ed08dbc11ac47cc20f2633b434aa9b04d3e4700
Franco Armando <francoarmando1911@gmail.com>
2024-11-24 21:00:15 -0300

    agregando el componente al reducer

--- src/components/FilterByCategory.tsx
+++ src/components/FilterByCategory.tsx
@@ -1,7 +1,31 @@
-
+import { categories } from "../data/categories";
+import { useBudget } from "../hooks/useBudget";
 
 export default function FilterByCategory() {
+
+    const {dispatch} = useBudget()
   return (
-    <div className="bg-white shadow-lg rounded-lg p-10">FilterByCategory</div>
+    <div className="bg-white shadow-lg rounded-lg p-10">
+        <form>
+            <div className="flex flex-col md:flex-row md:items-center gap-5">
+                <label htmlFor="category">Filtrar gastos</label>
+                <select  
+                    id="category"
+                    className="bg-slate-100 p-3 flex-1 rounded">
+                    <option value="">-- Todas las categorias --</option>
+                    {categories.map(category => (
+                        <option
+                            value={category.id}
+                            key={category.id}
+                        >
+
+                        </option>
+
+                    ))}
+                </select>
+
+            </div>
+        </form>
+    </div>
   )
 }
--- src/reducers/budget-reducer.ts
+++ src/reducers/budget-reducer.ts
@@ -1,4 +1,4 @@
-import { DraftExpense, Expense } from "../types";
+import { Category, DraftExpense, Expense } from "../types";
 import { v4 as uuidv4 } from "uuid";
 
 
@@ -10,13 +10,15 @@ export type BudgetActions =
     | { type: 'remove-expense', payload: { id: Expense['id'] } }
     | { type: 'get-expense-by-id', payload: { id: Expense['id'] } }
     | { type: 'update-expense', payload: { expense: Expense} }
-    | { type: 'reset-app'};
+    | { type: 'reset-app'}
+    | { type: 'add-filter-category', payload: {id: Category['id']} };
 
 export type BudgetState = {
     budget : number
     modal: boolean
     expenses: Expense[]
     editingId: Expense['id']
+    currentCategory: Category['id']
 }
 
 const initialBudget = () : number => {
@@ -33,7 +35,8 @@ export const initialState : BudgetState = {
     budget : initialBudget(),
     modal: false,
     expenses: localSotrageExpenses(),
-    editingId: ''
+    editingId: '',
+    currentCategory: ''
 }
 
 const createExpense = (draftExpense : DraftExpense) : Expense => {
@@ -115,6 +118,14 @@ export const budgetReducer = (
             expenses: [],
         }
         
+    }
+
+    if (action.type === 'add-filter-category') {
+        return {
+            ...state,
+            currentCategory: action.payload.id,
+        }
+
     }
     return state
 }
